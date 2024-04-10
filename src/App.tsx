b0fe78c2d20eb4f76f58814a19d4cc8fb1387b31
Kamil Mrówka <kamil31245@gmail.com>
2024-04-10 17:51:14 +0200

    Added a graph on the home page, started setting up the layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from "./pages/HomePage.tsx";
 import {Route, Routes} from "react-router-dom";
 import LoginPage from "./pages/LoginPage.tsx";
+import { Chart } from 'chart.js';
+
 
 function App() {
     return (
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,58 +1,93 @@
+import { Chart, LineController, CategoryScale, LinearScale, PointElement, LineElement } from "chart.js";
 import Navbar from "../components/Navbar.tsx";
-import {useEffect, useState} from "react";
-import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import { useEffect, useRef } from "react";
 export default function HomePage() {
-    
-    const navigate = useNavigate();
 
-    const [data, setData] = useState(null);
+    const chartRef = useRef<HTMLCanvasElement | null>(null);
 
-    
+Chart.register(LineController, CategoryScale, LinearScale, PointElement, LineElement);
+
+    const data = [
+        {day: 1.04, weight: 85.4},
+        {day: 2.04, weight: 84.7},
+        {day: 3.04, weight: 84.5},
+        {day: 4.04, weight: 85.0},
+        {day: 5.04, weight: 84.3},
+        {day: 6.04, weight: 84.0},
+        {day: 7.04, weight: 84.1}
+    ];
+
     useEffect(() => {
-        const getHomePageData =
-        async () => {
-            const token = localStorage.getItem('token');
-            const headers = {
-                'Authorization': `Bearer ${token}`
+        if (chartRef.current) {
+            const ctx = chartRef.current.getContext('2d');
+            if (ctx) {
+                new Chart(
+                    ctx,
+                    {
+                        type: "line",
+                        data: {
+                            labels: data.map(row => row.day),
+                            datasets: [
+                                {
+                                    label: 'Your weight this week',
+                                    data: data.map(row => row.weight),
+                                    borderColor: 'rgb(76, 187, 23)',
+                                }
+                            ]
+                        },
+                        options: {
+                            scales: {
+                                x: {
+                                    grid: {
+                                        color: 'rgba(70, 70, 70)'
+                                    },
+                                    ticks: {
+                                        color: 'white',
+                                },
+                                },
+                                y: {
+                                    grid: {
+                                        color: 'rgba(70, 70, 70)'
+                                    },
+                                    ticks: {
+                                        color: 'white', 
+                                        callback: function(value, index, ticks) {
+                                            return value + " kg";
+                                        }
+                                    },
+                                },
+                            },
+                        }
+                    }
+                )
             }
-            // axios.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem('token')
-            axios.get('http://localhost:8080/api/v1/week' + "?date=2024-01-01", {headers: headers}).then(response => {
-                setData(response.data);
-            }).catch(error => {
-                if (error.response.status === 403) {
-                    console.log("from home page")
-                    navigate('/login');
-                }
-            })
         }
-        getHomePageData();
     })
+
+    
+    
     return (
         <div className={"container-fluid flex-column p-0 align-self-start"}>
             <Navbar></Navbar>
-            <div className={"d-flex text-white p-5 justify-content-evenly align-items-center"}>
-                <div className={"p-1"}>
-                    You're logged in!
-                </div>
-                <div className={"p-1"}>
-                    <ul className={"list-group"}>
-                        <li className={"list-group-item"}>
-                            That's great
-                        </li>
-                        <li className={"list-group-item"}>
-                            But what is your median weight?
-                        </li>
-                        <li className={"list-group-item"}>
-                            I think I know the answer
-                        </li>
-                        <li className={"list-group-item"}>
-                            At least from this week
-                        </li>
-                        <li className={"list-group-item"}>
-                            It's: {data}kg
-                        </li>
-                    </ul>
+            <div className={"container-fluid d-flex text-white p-4 justify-content-evenly align-items-start"}>
+                <div className="flex-row d-flex container-fluid ps-3 pb-3 pe-3 justify-conent-evenly">
+                    <div className={"col-7 text-center"}>
+                        <div className="pb-3">
+                            <text> Your weight this week</text>
+                        </div>
+                        <canvas ref={chartRef} />
+                    </div>
+                    <div className="flex-column d-flex justify-content-center">
+                        <h2 className="mt-5">Save today's measurements:</h2>
+                        <div className="col-6">
+                            <div className="input-group">
+                                <input type="text" placeholder="weight" className="form-control m-2 text-light bg-dark"></input>
+                            </div>
+                            <div className="input-group">
+                                <input type="text" placeholder="calories" className="form-control m-2 text-light bg-dark"></input>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
